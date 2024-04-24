2050945e0cfbc5ed9dfaa459b54f5b3579feaad7
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2024-04-24 11:00:57 -0400

    week1

---
+++ my_script.js
@@ -0,0 +1,6 @@
+let h2 = document.querySelector('.test')
+h2.innerHTML =  `🎉 Congratulations! You are having a <strong>boy</strong>!`;
+/* Hello */
+const scores = [99, 88, 93, 70, 84, 69];
+const outreach = scores.map(total => total < 70);
+console.log(outreach);
